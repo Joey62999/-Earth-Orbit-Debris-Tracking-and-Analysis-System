e06673b9e83e73906949ff1a3c7870f6d7601b89
Joey62999 <jnassi@miners.utep.edu>
2025-04-18 02:38:08 -0600

    Update Sattelite.java

--- src/utep/oop/team22code/Sattelite.java
+++ src/utep/oop/team22code/Sattelite.java
@@ -1,5 +1,16 @@
 package utep.oop.team22code;
 
-public class Sattelite {
+public class Satellite extends SpaceObject {
 
+    public Satellite(String recordId, String satelliteName, String country, String orbitType,
+                     int launchYear, String launchSite, double longitude, double avgLongitude,
+                     String geohash, int daysOld, int conjunctionCount) {
+        super(recordId, satelliteName, country, orbitType, launchYear, launchSite,
+              longitude, avgLongitude, geohash, daysOld, conjunctionCount);
+    }
+
+    @Override
+    public String getObjectType() {
+        return "Satellite";
+    }
 }
