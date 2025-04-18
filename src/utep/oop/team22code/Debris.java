bf7a3a55483cdcc55803336bba9b3b7e5213f7ba
Joey62999 <jnassi@miners.utep.edu>
2025-04-18 02:37:00 -0600

    Update Debris.java

--- src/utep/oop/team22code/Debris.java
+++ src/utep/oop/team22code/Debris.java
@@ -1,5 +1,14 @@
-package utep.oop.team22code;
+public class Debris extends SpaceObject {
 
-public class Debris {
+    public Debris(String recordId, String satelliteName, String country, String orbitType,
+                  int launchYear, String launchSite, double longitude, double avgLongitude,
+                  String geohash, int daysOld, int conjunctionCount) {
+        super(recordId, satelliteName, country, orbitType, launchYear, launchSite,
+              longitude, avgLongitude, geohash, daysOld, conjunctionCount);
+    }
 
+    @Override
+    public String getObjectType() {
+        return "Debris";
+    }
 }
